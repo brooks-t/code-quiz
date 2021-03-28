a90588b568b296ed6760032e577bff5055f894e3
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-03-28 06:21:02 -0700

    added header with view highscores and timer. added code quiz challenge introduction

---
+++ script.js
@@ -0,0 +1,20 @@
+var secondsRemaining = 76;
+var startButton = document.querySelector("#start-button");
+var startQuiz = document.querySelector("#start-quiz");
+var countDown = document.querySelector("#count-down");
+
+function setTimer () {
+    var timer = setInterval(function() {
+        secondsRemaining--;
+        countDown.textContent = secondsRemaining;
+        if(secondsRemaining === 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+
+startButton.addEventListener("click", function () {
+    startQuiz.setAttribute("style", "display: none");
+    setTimer();
+})
+
