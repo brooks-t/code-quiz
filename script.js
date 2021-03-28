89f5e55947e7fb297e749f62fb7a79a5d7bd8bd7
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-03-28 06:39:56 -0700

    built array of objects containing TBD questions and answers

--- script.js
+++ script.js
@@ -3,6 +3,46 @@ var startButton = document.querySelector("#start-button");
 var startQuiz = document.querySelector("#start-quiz");
 var countDown = document.querySelector("#count-down");
 
+var quiz = [
+    {
+        question: "question1TBD",
+        answer1: "answer1-1TBD",
+        answer2: "answer1-2TBD",
+        answer3: "answer1-3TBD",
+        answer4: "answer1-4TBD"
+    },
+    {
+        question: "question2TBD",
+        answer1: "answer2-1TBD",
+        answer2: "answer2-2TBD",
+        answer3: "answer2-3TBD",
+        answer4: "answer2-4TBD"
+    },
+    {
+        question: "question3TBD",
+        answer1: "answer3-1TBD",
+        answer2: "answer3-2TBD",
+        answer3: "answer3-3TBD",
+        answer4: "answer3-4TBD"
+    },
+    {
+        question: "question4TBD",
+        answer1: "answer4-1TBD",
+        answer2: "answer4-2TBD",
+        answer3: "answer4-3TBD",
+        answer4: "answer4-4TBD"
+    },
+    {
+        question: "question5TBD",
+        answer1: "answer5-1TBD",
+        answer2: "answer5-2TBD",
+        answer3: "answer5-3TBD",
+        answer4: "answer5-4TBD"
+    },
+]
+
+console.log(quiz);
+
 function setTimer () {
     var timer = setInterval(function() {
         secondsRemaining--;
